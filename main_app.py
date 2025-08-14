484023e8842124e7c588d778a6dee4a5c3490524
Shaswat125 <shaswatanand125@gmail.com>
2025-08-14 22:00:54 +0530

    Saving Email Data to JSON files

--- main_app.py
+++ main_app.py
@@ -1,6 +1,14 @@
 import read_attachment_files
 import read_mails
 import os, pandas
+import json
+
+def save_all_into_file(data, output_path):
+    try:
+       with open(output_path, "w", encoding="utf-8") as f:
+            json.dump(data, f, indent=2, ensure_ascii=False, default=str)
+    except Exception as e:
+        print(f"An Error Occuring while saving data: {e}")
 
 
 if __name__ == "__main__":
@@ -11,9 +19,7 @@ if __name__ == "__main__":
     # print("Mail Data is:")
     # print("Body:\n", email_data.get('body'))
     # print("Attachment report:\n", email_data.get('attachment'))
-
     attachment_data=read_attachment_files.extract_all_files_from_folder("attachments")
-    for file_data in attachment_data:
-        text_content = file_data.get('text')
-        print(text_content)
-        print("\n" + "="*40 + "\n")
+    print(type(email_data), type(attachment_data))
+    save_all_into_file(email_data, "Email Data.json")
+    save_all_into_file(attachment_data, "Attachment Data.json")
