8eb5c0f14c77ac3d3aa6db8c58d42d0ea700b95a
Shaswat125 <shaswatanand125@gmail.com>
2025-08-15 22:10:59 +0530

    Testing Chat Response

--- main_app.py
+++ main_app.py
@@ -2,6 +2,7 @@ import read_attachment_files
 import read_mails
 import os
 import json
+from open_ai_call import ChatGPTWrapper
 
 def save_all_into_file(data, output_path):
     try:
@@ -13,10 +14,16 @@ def save_all_into_file(data, output_path):
 
 if __name__ == "__main__":
 
-    read_attachment_files.clear_all_files_folder("attachments")
-    file_path = "Project Alpha – Weekly Update and Action Items.msg"
-    email_data=read_mails.read_any_email(file_path)
-    attachment_data=read_attachment_files.extract_all_files_from_folder("attachments")
-    print(type(email_data), type(attachment_data))
-    save_all_into_file(email_data, "Email Data.json")
-    save_all_into_file(attachment_data, "Attachment Data.json")
+    # read_attachment_files.clear_all_files_folder("attachments")
+    # file_path = "emails/Project Alpha – Weekly Update and Action Items.msg"
+    # email_data=read_mails.read_any_email(file_path)
+    # attachment_data=read_attachment_files.extract_all_files_from_folder("attachments")
+    # print(type(email_data), type(attachment_data))
+    # save_all_into_file(email_data, "Email Data.json")
+    # save_all_into_file(attachment_data, "Attachment Data.json")
+    wrapper = ChatGPTWrapper(model="gpt-3.5-turbo")
+    response = wrapper.get_chat_response(
+        prompt="Tell me a joke",
+        system_prompt="You are a friendly and witty coder."
+    )
+    print(response)
