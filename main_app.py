b80b1a512034ca85d11aae82f26f5d86e89ca670
Shaswat125 <shaswatanand125@gmail.com>
2025-08-16 15:53:45 +0530

    Respond to Mails AI Agent

--- main_app.py
+++ main_app.py
@@ -5,23 +5,21 @@ import json
 from open_ai_call import ChatGPTWrapper
 from utils import generate_prompt, save_as_json
 
-
+ # AI Agent to respond to mails
 def respond_mails_AI_agent(mail_folder_path="emails"):
 
     # Iterate for all mails in folder to read, mails and attachments 
-    # AI Agent to respond to mails
-
     for filename in os.listdir(mail_folder_path):
         email_extensions = ('.eml', '.msg', '.mbox')
         if filename.endswith(email_extensions):
             file_path = os.path.join(mail_folder_path, filename)
-            print(f"{filename}\n")
+            print(f"For the mail : {filename}\n")
             read_att.clean_folder("attachments")
 
             # read email body
             email_data=read_mails.read_any_email(file_path)
             email_body=email_data.get('body')
-
+            print(f"Mail Body Reads: {email_body}\n")
             # read attachments
             attachment_data=read_att.extract_all_files_from_folder("attachments")
             attachment_merged=""
@@ -29,22 +27,23 @@ def respond_mails_AI_agent(mail_folder_path="emails"):
                 text_content = attachment.get('text')
                 attachment_merged+=str(text_content)
                 attachment_merged+=" - "
+            print(f"Attachment Reads: {attachment_merged}\n")
 
             engineered_prompt=generate_prompt(email_body, attachment_merged)
-            print(engineered_prompt)
+            print(f"Geneerated Prompt: {engineered_prompt}\n")
             save_as_json(email_data, "Email Data.json")
             save_as_json(attachment_data, "Attachment Data.json")
 
-            # wrapper = ChatGPTWrapper(model="gpt-3.5-turbo")
-            # response = wrapper.get_chat_response(
-            #     prompt=engineered_prompt,
-            #     system_prompt="You are a professional AI assistant for email replies. Use only the provided email and attachments to generate accurate, clear, and polite responses. Do not assume or hallucinate information. If data is missing, say so clearly."
-            # )
-            # print(response)
-            # response_json = json.dumps({
-            #     "email_response": response
-            # }, ensure_ascii=False, indent=2)
-            # save_as_json(response_json, "open ai response json Data.json")
+            wrapper = ChatGPTWrapper(model="gpt-3.5-turbo")
+            response = wrapper.get_chat_response(
+                prompt=engineered_prompt,
+                system_prompt="You are a professional AI assistant for email replies. Use only the provided email and attachments to generate accurate, clear, and polite responses. Do not assume or hallucinate information. If data is missing, say so clearly."
+            )
+            print(response)
+            response_json = json.dumps({
+                "email_response": response
+            }, ensure_ascii=False, indent=2)
+            save_as_json(response_json, "open ai response json Data.json")
 
 if __name__ == "__main__":
     respond_mails_AI_agent(mail_folder_path="emails")
